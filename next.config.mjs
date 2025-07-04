40ed15c50fec8b3fa2b37c0727090ecef5405024
chen-ryan1337 <ryanchen1337@gmail.com>
2025-07-04 14:15:19 -0400

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["oaidalleapiprodscus.blob.core.windows.net"],
-    domains: ["https://wpaysatiyftwgaoeubjh.supabase.co"],
+    domains: [
+      "oaidalleapiprodscus.blob.core.windows.net",
+      "https://wpaysatiyftwgaoeubjh.supabase.co",
+    ],
   },
 };
 
 export default nextConfig;
+
+// remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: '**.supabase.co', // Or your specific Supabase project URL
+//           pathname: '/storage/v1/object/public/**',
+//         },
+//       ],
